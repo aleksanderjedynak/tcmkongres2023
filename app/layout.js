a4fe59776a0e504f375cdf5ca213bf4f25e3faa6
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2023-05-13 21:04:48 +0200

    tylko formatowania

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'TCM Kongres 2023',
-  description: 'V Międzynarodowy Kongres Medycyny Chińskiej organizowany przez Polskie Towarzystwo Tradycyjnej Medycyny Chińskiej (PTTMC) w Krakowie.',
-}
+  title: "TCM Kongres 2023",
+  description:
+    "V Międzynarodowy Kongres Medycyny Chińskiej organizowany przez Polskie Towarzystwo Tradycyjnej Medycyny Chińskiej (PTTMC) w Krakowie.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
-      <body className={inter.className}>{children}</body>
+      <body data-free="true" className={inter.className}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
